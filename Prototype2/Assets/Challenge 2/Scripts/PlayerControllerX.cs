36775ac5c09538b22e95b16836670fa482a7ebb4
m1rakulus <145970806+m1rakulus@users.noreply.github.com>
2023-10-01 12:16:59 +0100

    Challenge2done

---
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float nextSpawn = 0.0f;
+    public float spawnDelay = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnDelay;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            
+        }
+    }
+}
